68fee1ade243c5e7b0e17a9a1d62535d6ec6818a
Jocelyn Wensloff <jewensloff@gmail.com>
2023-09-18 16:09:45 -0600

    feat: Display cocktail information to the DOM

--- src/Components/SingleCocktail/SingleCocktail.js
+++ src/Components/SingleCocktail/SingleCocktail.js
@@ -1,11 +1,12 @@
 import './SingleCocktail.css'
 
 function SingleCocktail ({name, ingredients, instructions}) {
-
+  let formattedIngredients = ingredients.join(' ')
   return (
     <div>
       <p>{name}</p>
-
+      <p>Ingredients: {formattedIngredients}</p>
+      <p>Instructions: {instructions}</p>
     </div>
   )
 }
