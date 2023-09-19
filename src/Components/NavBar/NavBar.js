55e52453119bad90891153dfcd12af0d2ee0de4c
Jocelyn Wensloff <jewensloff@gmail.com>
2023-09-18 18:07:08 -0600

    feat: Add initial styling to nav bar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,9 +1,11 @@
+import { NavLink } from 'react-router-dom';
 import './NavBar.css'
 
 function NavBar() {
   return (
-    <nav>
-      this is the nav bar
+    <nav className='nav-bar'>
+      <NavLink className='home-btn' to='/'>Home</NavLink>
+      <NavLink className='see-faves-btn 'to='/favorites'>Favorites</NavLink>
     </nav>
   )
 }
