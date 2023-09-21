e1ad205f369c218c1b149accbbc7997de2e2f462
Jocelyn Wensloff <jewensloff@gmail.com>
2023-09-21 07:02:19 -0600

    Feat: Add error message

--- src/Components/CocktailWrapper/CocktailWrapper.js
+++ src/Components/CocktailWrapper/CocktailWrapper.js
@@ -8,43 +8,60 @@ function CocktailWrapper({
   deleteFavorite,
   favorites,
 }) {
-  const cocktailComponents = cocktails.map((cocktail) => {
-    const keys = Object.keys(cocktail);
-    const ingredients = [];
-    const measurements = [];
-    let finalIngredients = [];
+  if (!cocktails) {
+    console.log('hello world');
+    return (
+      <div className='mesage-container container'>
+        <div className='outter-border container'>
+          <div className='inner-border container'>
+            <div className='error-message container'>
+              <p>Appologies, we recognize that cocktail</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    const cocktailComponents = cocktails.map((cocktail) => {
+      const keys = Object.keys(cocktail);
+      const ingredients = [];
+      const measurements = [];
+      let finalIngredients = [];
 
-    keys.map((key) => {
-      if (key.includes('Ingredient') && cocktail[key]) {
-        ingredients.push(cocktail[key]);
-      }
-      if (key.includes('strMeasure') && cocktail[key]) {
-        measurements.push(cocktail[key]);
-      }
-      measurements.map((measurement, index) => {
-        let ing = `${measurement} ${ingredients[index]}`;
-        if (!finalIngredients.includes(ing)) {
-          finalIngredients.push(ing);
+      keys.map((key) => {
+        if (key.includes('Ingredient') && cocktail[key]) {
+          ingredients.push(cocktail[key]);
         }
+        if (key.includes('strMeasure') && cocktail[key]) {
+          measurements.push(cocktail[key]);
+        }
+        measurements.map((measurement, index) => {
+          let ing = `${measurement} ${ingredients[index]}`;
+          if (!finalIngredients.includes(ing)) {
+            finalIngredients.push(ing);
+          }
+        });
+        return finalIngredients;
       });
-      return finalIngredients;
+
+      return (
+        <SingleCocktail
+          name={cocktail.strDrink}
+          ingredients={finalIngredients}
+          instructions={cocktail.strInstructions}
+          id={cocktail.idDrink}
+          img={cocktail.strDrinkThumb}
+          addToFavorites={addToFavorites}
+          deleteFavorite={deleteFavorite}
+          key={cocktail.idDrink}
+          favorites={favorites}
+        />
+      );
     });
 
     return (
-      <SingleCocktail
-        name={cocktail.strDrink}
-        ingredients={finalIngredients}
-        instructions={cocktail.strInstructions}
-        id={cocktail.idDrink}
-        img={cocktail.strDrinkThumb}
-        addToFavorites={addToFavorites}
-        deleteFavorite={deleteFavorite}
-        key={cocktail.idDrink}
-        favorites={favorites}
-      />
+      <section className='cocktail-wrapper'> {cocktailComponents} </section>
     );
-  });
-
-  return <section className='cocktail-wrapper'> {cocktailComponents} </section>;
+  }
 }
 export default CocktailWrapper;
