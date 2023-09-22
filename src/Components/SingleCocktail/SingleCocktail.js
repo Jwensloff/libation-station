f70b1848f2898c9d9dc3153acc80e51d5ce4cef3
Jocelyn Wensloff <jewensloff@gmail.com>
2023-09-22 09:29:07 -0600

    feat: Add toggle functionality for drink cards

--- src/Components/SingleCocktail/SingleCocktail.js
+++ src/Components/SingleCocktail/SingleCocktail.js
@@ -1,6 +1,7 @@
 import './SingleCocktail.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 <FontAwesomeIcon icon={faXmark} />;
 function SingleCocktail({
   name,
@@ -12,6 +13,8 @@ function SingleCocktail({
   deleteFavorite,
   favorites,
 }) {
+  const [expand, setExpand] = useState(false);
+
   let formattedIngredients = ingredients.map((ingredient) => {
     return <li key={ingredient}>{ingredient}</li>;
   });
@@ -33,39 +36,52 @@ function SingleCocktail({
     }
   };
 
+  const toggleInfo = () => {
+    const newState = expand ? false : true;
+    setExpand(newState);
+  };
+
   return (
     // <div className='mesage-container card'>
-      <div className='outter-card-border card'>
-        <div className='middle-card-border card'>
-          <div className='inner-card-border card'>
-            <div className='single-cocktail' id={id} key={id}>
-              <img className='img-container' src={img} alt={name} />
-              <div className='info-container'>
-                {checkIsFavorite(id) ? (
-                  <button
-                    className='favorite-btn'
-                    onClick={() => deleteFavorite(id)}
-                  >
-                    <FontAwesomeIcon icon={faXmark} />
-                  </button>
-                ) : (
-                  <button
-                    className='favorite-btn'
-                    onClick={() => addToFavorites(newCocktail)}
-                  >
-                    <FontAwesomeIcon icon={faStar} />
-                  </button>
-                )}
-                <h2 className='name'>{name}</h2>
-                {/* <ul className='ingredients-list'>
-                  Ingredients: {formattedIngredients}
-                </ul>
-                <p>Instructions: {instructions}</p> */}
-              </div>
+    <div className='outter-card-border card'>
+      <div className='middle-card-border card'>
+        <div className='inner-card-border card'>
+          <div className='single-cocktail' id={id} key={id}>
+            <img className='img-container' src={img} alt={name} />
+            <div className='info-container'>
+              {checkIsFavorite(id) ? (
+                <button
+                  className='favorite-btn'
+                  onClick={() => deleteFavorite(id)}
+                >
+                  <FontAwesomeIcon icon={faXmark} />
+                </button>
+              ) : (
+                <button
+                  className='favorite-btn'
+                  onClick={() => addToFavorites(newCocktail)}
+                >
+                  <FontAwesomeIcon icon={faStar} />
+                </button>
+              )}
+              <h2 className='name'>{name}</h2>
+              <button onClick={() => toggleInfo()}>Toggle</button>
+              {expand && (
+                <>
+                  <ul className='ingredients-list'>
+                    <span className='span'>Ingredients:</span>{' '}
+                    {formattedIngredients}
+                  </ul>
+                  <p>
+                    <span className='span'>Instructions:</span> {instructions}
+                  </p>
+                </>
+              )}
             </div>
           </div>
         </div>
       </div>
+    </div>
     // </div>
   );
 }
